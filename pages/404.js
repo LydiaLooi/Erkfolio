f7218ffd0d6e05851610e21a299499d0bf76e89e
Lydia Looi <itslydia.looi@gmail.com>
2023-04-14 12:31:26 +1200

    Added custom error pages for 404 and 500

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout"
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <div>
+                <h1>404 - Page Not Found</h1>
+                <p>What are you even looking for?
+                </p>
+            </div>
+        </Layout>
+    )
+}
---
+++ pages/500.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+// pages/500.js
+export default function Custom500() {
+    return (
+        <Layout>
+            <h1>500 - Server-side error occurred</h1>
+            <p>Oops...</p>
+        </Layout>
+    )
+}
