a6efd796692489260f6346f00037c8aefb59c662
Lydia Looi <itslydia.looi@gmail.com>
2023-04-21 16:41:28 +1200

    Typescriptified everything!

--- collection_names.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    artCollection: process.env.ART_COLLECTION || "art-dev",
-    tagsCollection: process.env.TAG_COLLECTION || "filters-dev",
-    projectCollection: process.env.PROJECT_COLLECTION || "projects-dev",
-}
---
+++ collection_names.ts
@@ -0,0 +1,4 @@
+
+export const artCollection = process.env.ART_COLLECTION || "art-dev";
+export const tagsCollection = process.env.TAG_COLLECTION || "filters-dev";
+export const projectCollection = process.env.PROJECT_COLLECTION || "projects-dev";
