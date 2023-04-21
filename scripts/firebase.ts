8a46cea4a45e934d7cbfe0bf37b94b7929ca1fcb
Lydia Looi <itslydia.looi@gmail.com>
2023-04-21 19:33:29 +1200

    Updated packages. Hopefully made firebase iframe not load immediately

--- scripts/firebase.ts
+++ scripts/firebase.ts
@@ -1,7 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore/lite';
+
+import { initializeAuth, indexedDBLocalPersistence, browserLocalPersistence, browserSessionPersistence, browserPopupRedirectResolver, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+import { getLogger } from "../logging/log-util";
+
+const logger = getLogger("Firebase")
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -16,12 +21,22 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
+// export const auth = getAuth();
+export const auth = initializeAuth(app, {
+    persistence: [
+        indexedDBLocalPersistence,
+        browserLocalPersistence,
+        browserSessionPersistence
+    ],
+});
+
+
 const provider = new GoogleAuthProvider();
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export function signInWithGoogle() {
-    signInWithPopup(auth, provider)
+    // signInWithPopup(auth, provider)
+    signInWithPopup(auth, provider, browserPopupRedirectResolver).then(() => { logger.info("Logged in successfully") }).catch((e) => { logger.warn("Cancelled popup") }) // https://github.com/firebase/firebase-js-sdk/issues/4946#issuecomment-921147692
 }
